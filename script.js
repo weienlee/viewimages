1c93fab8a364700b1a1823f5d2032a307d860ff2
Wei-En Lee <weienlee@mit.edu>
2018-02-16 00:57:24 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).on('click', '.rg_ic.rg_i', function(event) {
+  handleClick(event);
+});
+
+function handleClick(event) {
+  const img = event.target;
+  const container = img.closest('div');
+  let meta = $(container).find('.notranslate')[0].innerHTML;
+  meta = JSON.parse(meta);
+  const url = meta.ou;
+  insertButton(url);
+}
+
+function insertButton(url) {
+  const section = $('#irc_cc');
+  const layers = section[0].children;
+
+  let layer;
+  for (let i=0; i<layers.length; i++) {
+    if (layers[i].style.transform === "translate3d(0px, 0px, 0px)") {
+      layer = layers[i];
+    }
+  }
+
+  const table = $(layer).find('table.irc_but_r')[0];
+  const tr = $(table).find('tr');
+  $('.view-image-td').remove();
+  tr.prepend(renderButton(url));
+}
+
+function renderButton(url) {
+  return $(`
+    <td class="view-image-td">
+      <a
+        class="view-image-button"
+        href="` + url + `"
+        target="_blank"
+      >
+        <span>View Image</span>
+      </a>
+    </td>
+  `);
+}
