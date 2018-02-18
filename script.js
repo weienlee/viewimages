016af1d87f3d68f9249b3eff1aca140d55d15817
Federico Brigante <github@bfred.it>
2018-02-19 04:37:11 +0700

    De-jQuerify

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
-$(document).on('click', '.rg_ic.rg_i', function(event) {
-  handleClick(event);
-});
+document.addEventListener('click', handleClick);
 
 function handleClick(event) {
-  const img = event.target;
-  const container = img.closest('div');
-  let meta = $(container).find('.notranslate')[0].innerHTML;
-  meta = JSON.parse(meta);
+  const img = event.target.closest('.rg_ic.rg_i');
+  if (!img) {
+    return;
+  }
+  const json = img.closest('div').querySelector('.notranslate').innerHTML;
+  const meta = JSON.parse(json);
   const url = meta.ou;
   insertButton(url);
 }
 
 function insertButton(url) {
-  const section = $('#irc_cc');
-  const layers = section[0].children;
+  const section = document.querySelector('#irc_cc');
+  const layer = section.querySelector('[style*="translate3d(0px, 0px, 0px)"]');
 
-  let layer;
-  for (let i=0; i<layers.length; i++) {
-    if (layers[i].style.transform === "translate3d(0px, 0px, 0px)") {
-      layer = layers[i];
-    }
+  const tr = layer.querySelector('table.irc_but_r tr');
+  if(!tr.querySelector('.view-image-td')) {
+    tr.insertAdjacentHTML('afterbegin', renderButton(url));
   }
-
-  const table = $(layer).find('table.irc_but_r')[0];
-  const tr = $(table).find('tr');
-  $('.view-image-td').remove();
-  tr.prepend(renderButton(url));
 }
 
 function renderButton(url) {
-  return $(`
+  return `
     <td class="view-image-td">
       <a
         class="view-image-button"
-        href="` + url + `"
+        href="${url}"
         target="_blank"
       >
         <span>View Image</span>
       </a>
     </td>
-  `);
+  `;
 }
